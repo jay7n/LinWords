93ed79f166922fa07e7fb884d9dab4979aaba856
lookof <lookof@gmail.com>
2016-01-21 13:29:14 +0800

    a very simple db is made&connected

--- hello_tornado.py
+++ hello_tornado.py
@@ -18,11 +18,51 @@ class MyHandler(tornado.web.RequestHandler):
         self.write("welcome to my tornado domain!\n")
         #self.write(j)
 
+def _wrap_word(word):
+    dict = {
+        'word' : word,
+        'explain' : 'test hello 测试'
+    }
+
+    return dict
+
+class WordStore(object):
+    __db = None
+
+    @classmethod
+    def init(cls, host = "youchun.li", port = 27017):
+        if not cls.__db:
+            from pymongo import MongoClient
+            client = MongoClient(host, port)
+            cls.__db = client['wordstore-database']
+            coll = cls.__db['coll']
+
+    def __init__(self):
+        WordStore.init()
+
+    def GetWord(self, word):
+        coll = self.__class__.__db['coll']
+        item = coll.find_one({'word' : word})
+        if not item:
+            print "%s not found" %word
+            new_item = _wrap_word(word)
+            coll.insert_one(new_item)
+            item = new_item
+
+        from bson import json_util as jutil
+        return jutil.dumps(item)
+
+_wordStore = WordStore()
+
 class WordHandler(tornado.web.RequestHandler):
     def get(self):
         word = self.request.path[1:]
-        self.write(word)
-        self.write("女孩")
+        # self.write(word)
+        # self.write("女孩")
+        res = _wordStore.GetWord(word)
+        print res
+        self.write(res)
+
 
 if __name__ == "__main__":
     app = tornado.web.Application([
@@ -30,5 +70,5 @@ if __name__ == "__main__":
         (r"/[\w\s]+", WordHandler),
     ])
 
-    app.listen(8080)
+    app.listen(8000)
     tornado.ioloop.IOLoop.instance().start()
